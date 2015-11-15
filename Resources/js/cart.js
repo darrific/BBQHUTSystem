5ea54d6a0f08f93aaeece81abbdc16f2d17942b9
sayaad <sayaad@hotmail.com>
2015-11-15 18:51:23 -0400

    Removed Clock timepicker

--- Resources/js/cart.js
+++ Resources/js/cart.js
@@ -2,17 +2,21 @@
 
 //Animations and Aesthetics
 $('#NextButton').click(function(){
-	$('#AddSideContainer').fadeOut(600);
-	$('#OrderInformationContainer').css('display','initial');
+	$('#AddSideContainer').fadeOut(500);
 	$('#OrderInformationContainer').fadeOut(0);
-	$('#OrderInformationContainer').fadeIn(600);
+	$('#OrderInformationContainer').css('display','initial');
+	$('#OrderInformationContainer').fadeIn(500);
 })
 
-$('#time').clockpicker({
-	default: 'now',
-	fromnow: 3600,
-	donetext: 'Choose',
-	autoclose: true
+$('#time').timepicker({
+	closeText: "Choose",
+	timeFormat: "hh:mm tt",
+	timeOnlyTitle: "Choose a pickup time",
+	stepMinute: 5,
+	hourMin: 8,
+	hourMax: 22,
+	showButtonPanel: false,
+	timeInput: true
 });
 
-$('#cell').mask("999-9999",{placeholder:"   -    "});
+$('#cell').mask("999-9999",{placeholder:"___-____"});
